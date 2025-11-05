9be40b222cb067fc9a6299a2d3fb0699e3a97922
mikshasa0207 <samikshajangid30@gmail.com>
2025-11-05 19:13:25 +0530

    basic server created

---
+++ apps/backend/index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+require("dotenv").config();
+
+const app = express();
+const PORT = process.env.SERVER_PORT;
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.status(200).send("<h1>Backend Running Successfully 🚀</h1>");
+});
+
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on port ${PORT}`);
+  console.log(`✅ Local Backend URL: ${process.env.BACKEND_LOCAL_URL}`);
+  console.log(`✅ Deployed Backend URL: ${process.env.BACKEND_SERVER_URL}`);
+});
