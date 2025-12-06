1bee9d7ce2b540a82b3fbb173f1e73847d591c4f
mikshasa0207 <samikshajangid30@gmail.com>
2025-12-06 23:03:43 +0530

    updated config with cors and cloudinary

--- apps/backend/config/cors.js
+++ apps/backend/config/cors.js
@@ -2,23 +2,22 @@ const cors = require("cors");
 require("dotenv").config();
 
 const allowedOrigins = [
-    process.env.FRONTEND_LOCAL_URL,
-    process.env.FRONTEND_SERVER_URL,
+  process.env.FRONTEND_LOCAL_URL,
+  process.env.FRONTEND_SERVER_URL,
 ];
 
 const corsOptions = {
-    origin: function (origin, callback) {
-        if (!origin || allowedOrigins.includes(origin)) {
-            callback(null, true);
-        } 
-        else {
-            console.warn(`❌ Blocked by CORS: ${origin}`);
-            callback(new Error("Not allowed by CORS"));
-        }
-    },
-    credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      console.warn("❌ Blocked by CORS:", origin);
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+  allowedHeaders: ["Content-Type", "Authorization"],
 };
 
 module.exports = cors(corsOptions);
