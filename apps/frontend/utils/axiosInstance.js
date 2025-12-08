78fcd86bac7e97605a12efbc56551cb03585c28c
mikshasa0207 <samikshajangid30@gmail.com>
2025-12-08 11:57:30 +0530

    frontend axiosinstance fixed

--- apps/frontend/utils/axiosInstance.js
+++ apps/frontend/utils/axiosInstance.js
@@ -1,9 +1,6 @@
 import axios from "axios";
 
-const BASE_URL =
-  process.env.NODE_ENV === "production"
-    ? process.env.NEXT_PUBLIC_BACKEND_SERVER_URL
-    : process.env.NEXT_PUBLIC_BACKEND_LOCAL_URL;
+const BASE_URL = process.env.NEXT_PUBLIC_BACKEND_SERVER_URL || "";
 
 const instance = axios.create({
   baseURL: BASE_URL,
